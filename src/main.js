67fbed78e49dcc60122d85bdf969bc6b796b1d61
tymur_lykho <tima.likho@gmail.com>
2025-04-03 14:07:03 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/about-me';
+import './js/covers';
+import './js/faq';
+import './js/projects';
+import './js/reviews';
+import './js/work-together';
+import './js/modal-window';
