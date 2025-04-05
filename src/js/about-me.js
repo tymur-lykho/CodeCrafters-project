bdb1cc0c47003013dce647ae177d397cacf8ac4e
Tetiana Taratula <ttaratula@gmail.com>
2025-04-05 16:57:49 +0100

    section About-me

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,1 +1,30 @@
+import accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+
+  const items = document.querySelectorAll(".accordion-item");
+  items.forEach((item) => {
+    const header = item.querySelector(".accordion-header");
+    header.addEventListener("click", () => {
+      items.forEach((i) => i.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+
+//   // Swiper
+//   const swiper = new Swiper(".swiper", {
+//     loop: true,
+//     slidesPerView: 3,
+//     spaceBetween: 20,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     keyboard: {
+//       enabled: true,
+//       onlyInViewport: true,
+//     },
+//     a11y: true,
+//     grabCursor: true,
+//   });
 
