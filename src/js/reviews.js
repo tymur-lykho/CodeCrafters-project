12924b40074e7fd6104c25a03c48dd35d03075f9
Olena Morozova <appsapps530@gmail.com>
2025-04-06 20:47:47 +0300

    add buttons, messages about server error

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,23 +1,56 @@
 import axios from "axios";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 import Swiper from 'swiper';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation, Keyboard } from "swiper/modules";
+import 'swiper/css/mousewheel'; 
+import {Keyboard } from "swiper/modules";
 
 
 const reviewsList = document.querySelector(".reviews-list");
+const prevButton = document.querySelector('.reviews-btn-prev');
+const nextButton = document.querySelector('.reviews-btn-next');
 
-axios.get('https://portfolio-js.b.goit.study/api/reviews')
-  .then(response => { createReviews(response.data); })
-  .catch(error => console.log(error))
+let swiper;
+
+function loadReviews() {
+  axios.get('https://portfolio-js.b.goit.study/api/reviews')
+    .then(response => {
+      createReviews(response.data);
+    })
+    .catch(error => {
+      iziToast.warning({
+        title: 'We are sorry',
+        message: 'Failed to load reviews. Please try again later.',
+        position: 'center',
+        timeout: 5000,
+      });
+        console.error('Помилка при отриманні відгуків:', error);
+        reviewsList.insertAdjacentHTML("beforeend", `<li class="review-item error">Not found</li>`);
+    });
+}
+
+// обсервер
+const reviewsSection = document.querySelector('#reviews-section');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadReviews(); 
+      observer.unobserve(reviewsSection); 
+    }
+  });
+}, { threshold: 0.5 }); 
+
+observer.observe(reviewsSection);
+
 
 function createReviews(reviews) {      
   const markup = reviews
     .map(review => {
       return `
       <li class="swiper-slide review-item">
-        <img class="review-img" src="${review.avatar_url}" alt="reviewer's avatar" width="48" height="48" />
+        <img class="review-img" src="${review.avatar_url}" alt="reviewer's avatar" width="48" height="48"  />
         <h3 class="subtitle review-author">${review.author}</h3>
         <p class="text review-text">${review.review}</p>
       </li>
@@ -31,19 +64,54 @@ function createReviews(reviews) {
 }
 
 function initSwiper() {
-  const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Keyboard],
-    slidesPerView: 4, 
+  swiper = new Swiper('.swiper', {
+    modules: [Keyboard],
+    slidesPerView: 1, 
     spaceBetween: 16, 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    loop: false,
     keyboard: {
       enabled: true,
       onlyInViewport: true,
-    },
+      },
+     mousewheel: {
+    forceToAxis: true, 
+    releaseOnEdges: true, 
+      },    
+  grabCursor: true,
+      breakpoints: {
+          768: {
+        slidesPerView: 2,
+          },
+          1440: {
+              slidesPerView: 4,              
+          }
+}
+  });    
+    
+    prevButton.addEventListener('click', () => {
+      swiper.slidePrev();
+      updateButtonState();
+  }); 
+
+   nextButton.addEventListener('click', () => {
+       swiper.slideNext();
+       updateButtonState();
   });
+
+swiper.on('slideChange', updateButtonState);
+
+updateButtonState();
+  
+}
+
+
+function updateButtonState() { 
+
+  prevButton.disabled = swiper.isBeginning; 
+  prevButton.classList.toggle('disabled', swiper.isBeginning); 
+
+  nextButton.disabled = swiper.isEnd; 
+  nextButton.classList.toggle('disabled', swiper.isEnd); 
 }
 
 
@@ -52,44 +120,5 @@ function initSwiper() {
 
 
 
-/* import axios from "axios";  
-import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation } from "swiper/modules";
 
-const reviewsList = document.querySelector(".reviews-list");
 
-axios.get('https://portfolio-js.b.goit.study/api/reviews?')
-  .then(response => { createReviews(response.data); })
-	.catch(error => console.log(error))
-
-function createReviews(reviews) {
-      const markup = reviews
-        .map(review => {
-            return `
-      <li class="swiper-slide review-item">
-            <img src="${review.avatar_url}" alt="reviewer's avatar" width="48" height="48" />
-            <h3 class="subtitle reviews-author">${review.author}</h3>
-            <p class="text reviews-text">${review.review}</p>
-          </li>
-            `;
-        })    
-        .join("");
-    reviewsList.insertAdjacentHTML("beforeend", markup);
-}
-
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation],
-  slidesPerView: 4, // Скільки карток видно одночасно
-  spaceBetween: 20, // Відстань між слайдами
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-  },
-}); */
