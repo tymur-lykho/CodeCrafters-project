479c3cf332750beb38086977254a31ca09e16ec1
tymur_lykho <tima.likho@gmail.com>
2025-04-07 12:10:25 +0300

    fix sbout section

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,49 +1,46 @@
-import accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+import accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-document.querySelectorAll('.accordion-item').forEach((item) => {
+document.querySelectorAll('.accordion-item').forEach(item => {
   const button = item.querySelector('.accordion-header');
   const icon = item.querySelector('.accordion-icon');
   const body = item.querySelector('.accordion-body');
 
   // const setArrow = (iconEl, isOpen) => {
-  //   iconEl.src = isOpen 
-  //     ? '../img/about-me/arrow-up.svg' 
+  //   iconEl.src = isOpen
+  //     ? '../img/about-me/arrow-up.svg'
   //     : '../img/about-me/arrow-down.svg';
   // };
 
-  setArrow(icon, item.classList.contains('active'));
+  // setArrow(icon, item.classList.contains('active')); comment by Tymur
 
-  button.addEventListener('click', (e) => {
-    e.stopPropagation();
-    const isActive = item.classList.contains('active');
+  // button.addEventListener('click', e => {
+  //   e.stopPropagation();
+  //   const isActive = item.classList.contains('active');
 
-    document.querySelectorAll('.accordion-item').forEach((el) => {
-      el.classList.remove('active');
-      const elIcon = el.querySelector('.accordion-icon');
-      if (elIcon) setArrow(elIcon, false);
-    });
+  //   document.querySelectorAll('.accordion-item').forEach(el => {
+  //     el.classList.remove('active');
+  //     const elIcon = el.querySelector('.accordion-icon');
+  //     if (elIcon) setArrow(elIcon, false);
+  //   });
 
-    if (!isActive) {
-      item.classList.add('active');
-      setArrow(icon, true);
-    }
-  });
+  //   if (!isActive) {
+  //     item.classList.add('active');
+  //     setArrow(icon, true);
+  //   }
+  // });
 
-  body.addEventListener('click', () => {
-    if (item.classList.contains('active')) {
-      item.classList.remove('active');
+  // body.addEventListener('click', () => {
+  //   if (item.classList.contains('active')) {
+  //     item.classList.remove('active');
 
-      
-      document.querySelectorAll('.accordion-item').forEach((el) => {
-        const elIcon = el.querySelector('.accordion-icon');
-        if (elIcon) setArrow(elIcon, el.classList.contains('active'));
-      });
-    }
-  });
+  //     document.querySelectorAll('.accordion-item').forEach(el => {
+  //       const elIcon = el.querySelector('.accordion-icon');
+  //       if (elIcon) setArrow(elIcon, el.classList.contains('active'));
+  //     });
+  //   }
+  // });
 });
-
-
 
 //   // Swiper
 //   const swiper = new Swiper(".swiper", {
@@ -61,4 +58,3 @@ document.querySelectorAll('.accordion-item').forEach((item) => {
 //     a11y: true,
 //     grabCursor: true,
 //   });
-
